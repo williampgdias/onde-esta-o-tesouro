2cf8cc3621645d52f05247cbbf9cd14c82b5694b
William Dias <smdwilliam89@gmail.com>
2025-05-29 11:39:20 -0300

    feat: Desabilitando o usuário de continuar o jogo após vencer.

--- onde-esta-o-tesouro-js-puro/script.js
+++ onde-esta-o-tesouro-js-puro/script.js
@@ -6,7 +6,46 @@ const posicaoTesouro = {
     coluna: Math.floor(Math.random() * colunas),
 };
 
-console.log(posicaoTesouro);
+function handleClickCelula(e) {
+    const celulaClicadaElement = e.currentTarget;
+    const linhaClicada = parseInt(celulaClicadaElement.dataset.linha);
+    const colunaClicada = parseInt(celulaClicadaElement.dataset.coluna);
+
+    if (
+        linhaClicada === posicaoTesouro.linha &&
+        colunaClicada === posicaoTesouro.coluna
+    ) {
+        celulaClicadaElement.classList.add('tesouro');
+        setTimeout(() => {
+            alert('Parabéns! Você encontrou o tesouro!');
+        }, 1000);
+
+        desabilitarCliques();
+    } else {
+        const distanciaTesouro =
+            Math.abs(linhaClicada - posicaoTesouro.linha) +
+            Math.abs(colunaClicada - posicaoTesouro.coluna);
+
+        if (distanciaTesouro >= 6) {
+            this.classList.add('frio');
+        } else if (distanciaTesouro >= 4) {
+            this.classList.add('morno');
+        } else if (distanciaTesouro >= 2) {
+            this.classList.add('quente');
+        } else if (distanciaTesouro === 1) {
+            this.classList.add('muito-quente');
+        }
+        console.log(distanciaTesouro);
+    }
+}
+
+function desabilitarCliques() {
+    const todasAsCelulas = document.querySelectorAll('.celula');
+    todasAsCelulas.forEach((celula) => {
+        celula.removeEventListener('click', handleClickCelula);
+        celula.style.cursor = 'not-allowed';
+    });
+}
 
 for (let linha = 0; linha < linhas; linha++) {
     for (let coluna = 0; coluna < colunas; coluna++) {
@@ -16,34 +55,6 @@ for (let linha = 0; linha < linhas; linha++) {
         celulaHTML.dataset.coluna = coluna;
         tabuleiro.appendChild(celulaHTML);
 
-        celulaHTML.addEventListener('click', function () {
-            const linhaClicada = parseInt(this.dataset.linha);
-            const colunaClicada = parseInt(this.dataset.coluna);
-
-            if (
-                linhaClicada === posicaoTesouro.linha &&
-                colunaClicada === posicaoTesouro.coluna
-            ) {
-                this.classList.add('tesouro');
-                setTimeout(() => {
-                    alert('Parabéns! Você encontrou o tesouro!');
-                }, 1000);
-            } else {
-                const distanciaTesouro =
-                    Math.abs(linhaClicada - posicaoTesouro.linha) +
-                    Math.abs(colunaClicada - posicaoTesouro.coluna);
-
-                if (distanciaTesouro >= 6) {
-                    this.classList.add('frio');
-                } else if (distanciaTesouro >= 4) {
-                    this.classList.add('morno');
-                } else if (distanciaTesouro >= 2) {
-                    this.classList.add('quente');
-                } else if (distanciaTesouro === 1) {
-                    this.classList.add('muito-quente');
-                }
-                console.log(distanciaTesouro);
-            }
-        });
+        celulaHTML.addEventListener('click', handleClickCelula);
     }
 }
